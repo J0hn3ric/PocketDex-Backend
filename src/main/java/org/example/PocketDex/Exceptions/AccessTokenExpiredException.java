a35425340459da91b16f63ea1cd749701fc0959f
je <john.eric.arriola@gmail.com>
2025-10-02 10:17:02 +0200

    fixed user controller issues

--- src/main/java/org/example/PocketDex/Exceptions/AccessTokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package org.example.PocketDex.Exceptions;
-
-public class AccessTokenExpiredException extends AuthException{
-    public AccessTokenExpiredException() {
-        super("Access token expired or invalid, request new access token with refresh token");
-    }
-}
--- src/main/java/org/example/PocketDex/Exceptions/RefreshTokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package org.example.PocketDex.Exceptions;
-
-public class RefreshTokenExpiredException extends AuthException{
-    public RefreshTokenExpiredException() {
-        super("Refresh token expired or invalid, make anonymous log in to supabase");
-    }
-}
